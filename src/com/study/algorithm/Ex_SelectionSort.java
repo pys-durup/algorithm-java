023e3847e8b32730c59e65de771dcb00dd6fecec
durup <enenfnq@gmail.com>
2021-04-06 20:55:58 +0900

    선택정렬 코드 작성

--- src/com/study/algorithm/Ex_SelectionSort.java
+++ src/com/study/algorithm/Ex_SelectionSort.java
@@ -1,10 +1,41 @@
 package com.study.algorithm;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class Ex_SelectionSort {
 
     public static void main(String[] args) {
 
-        System.out.println("git test");
+        caseOne();
 
+    }
+
+    private static void caseOne() {
+        Integer[] arrays = {14, 2, 5, 7, 9, 20, 11, 1, 8, 1, 1};
+        ArrayList<Integer> list = new ArrayList<>(Arrays.asList(arrays));
+        // System.out.println(list.toString());
+
+        int size = list.size();
+        ArrayList<Integer> resultArray = new ArrayList();
+        for (int i = 0; i < size; i++) {
+            resultArray.add(list.get(minNumber(list)));
+            list.remove(minNumber(list));
+        }
+
+        System.out.println(resultArray);
+    }
+
+    // 가장 작은 원소의 인덱스값 반환
+    private static int minNumber(ArrayList<Integer> list) {
+        int minNum = list.get(0);
+        int minIndex = 0;
+        for (int i = 1; i < list.size() ; i++) {
+            if (minNum > list.get(i)) {
+                minNum = list.get(i);
+                minIndex = i;
+            }
+        }
+        return minIndex;
     }
 }
