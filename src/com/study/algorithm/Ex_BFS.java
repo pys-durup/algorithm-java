9dc3a4a7c93034be12ad3d070d6b8e5e30b75b36
pys-durup <enenfnq@gmail.com>
2021-04-12 22:14:51 +0900

    BFS 작성중

---
+++ src/com/study/algorithm/Ex_BFS.java
@@ -0,0 +1,15 @@
+package com.study.algorithm;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class Ex_BFS {
+    public static void main(String[] args) {
+        Queue<String> queue = new LinkedList<>();
+
+    }
+
+    void bfs() {
+
+    }
+}
