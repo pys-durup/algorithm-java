cc834afc29aac5c8965bab076a01ba52b626957f
pys-durup <enenfnq@gmail.com>
2021-04-21 18:57:02 +0900

    프로그래머스 lv1 콜라츠 추측

---
+++ src/com/study/programmerslv1/Collatz.java
@@ -0,0 +1,26 @@
+package com.study.programmerslv1;
+
+import java.util.Scanner;
+
+public class Collatz {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int n = solution(scan.nextInt());
+        System.out.println(n);
+    }
+
+    private static int solution(int num) {
+        int count = 0;
+        while (true) {
+            if (count > 500) return -1;
+            if (num == 1) return count;
+
+            if (num % 2 == 0) {
+                num = num / 2;
+            } else if (num % 2 == 1) {
+                num = num * 3 + 1;
+            }
+            count++;
+        }
+    }
+}
