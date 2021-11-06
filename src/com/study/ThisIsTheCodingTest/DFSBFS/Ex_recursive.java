3fb20b8de5cb72a1cdbdbb0cf518e92f66a6b36f
pys-durup <enenfnq@gmail.com>
2021-11-06 21:22:57 +0900

    DFS/BFS recursive 예제

---
+++ src/com/study/ThisIsTheCodingTest/DFSBFS/Ex_recursive.java
@@ -0,0 +1,36 @@
+package com.study.ThisIsTheCodingTest.DFSBFS;
+
+public class Ex_recursive {
+
+    public static void recursiveFunction(int i) {
+        // 100번째 호출을 했을 때 종료되도록 종료 조건 명시
+        if (i == 100) return;
+        System.out.println(i + "번째 재귀 함수에서 " + (i + 1) + "번째 재귀함수를 호출합니다");
+        recursiveFunction(i + 1);
+        System.out.println(i + "번째 재귀 함수를 종료합니다");
+    }
+
+    // 반복적으로 구현한 n!
+    public static int factorialIterative(int n) {
+        int result = 1;
+        // 1부터 n까지의 수를 차례대로 곱하기
+        for (int i = 1; i <= n; i++) {
+            result *= i;
+        }
+        return result;
+    }
+
+    // 재귀적으로 구현한 n!
+    public static int factorialRecursive(int n) {
+        // n이 1 이하인 경우 1을 반환
+        if (n <= 1) return 1;
+        // n! = n * (n - 1)!를 그대로 코드로 작성
+        return n * factorialRecursive(n - 1);
+    }
+
+    public static void main(String[] args) {
+//        recursiveFunction(1);
+        System.out.println("반복적으로 구현:" + factorialIterative(5));
+        System.out.println("재귀적으로 구현:" + factorialRecursive(5));
+    }
+}
