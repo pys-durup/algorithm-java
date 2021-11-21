150926caeca9723753f9036b7fb64b2203bc9e5f
pys-durup <enenfnq@gmail.com>
2021-11-21 23:53:18 +0900

    정렬 퀵정렬 예제_2

--- src/com/study/ThisIsTheCodingTest/Sorting/QuickSort.java
+++ src/com/study/ThisIsTheCodingTest/Sorting/QuickSort.java
@@ -47,4 +47,29 @@ public class QuickSort {
         quickSort(arr, right + 1, end);
 
     }
+
+    private static void quickSort2(int[] arr, int start, int end) {
+        if (start >= end) return;
+        int pivot = start;
+        int left = start + 1;
+        int right = end;
+
+        while (left <= right) {
+            while (left <= end && arr[left] <= arr[pivot]) left++;
+            while (right > start && arr[right] >= arr[pivot]) right--;
+
+            if (left > right) {
+                int temp = arr[pivot];
+                arr[pivot] = arr[right];
+                arr[right] = temp;
+            }
+            else {
+                int temp = arr[left];
+                arr[left] = arr[right];
+                arr[right] = temp;
+            }
+        }
+        quickSort(arr, start, right - 1);
+        quickSort(arr, right + 1, end);
+    }
 }
